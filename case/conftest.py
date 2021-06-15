35fd767cd8f0d14e1599d24d7f7e13912b3f528e
624605062 <624605062@qq.com>
2021-06-15 16:42:27 +0800

    Initial commit

---
+++ case/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from page.login_page import _login
+
+"""
+case/conftest.py文件
+登录功能调用
+"""
+
+@pytest.fixture(scope='session')
+def login(driver, host):
+    """登录功能fixture"""
+    _login(driver, host)
